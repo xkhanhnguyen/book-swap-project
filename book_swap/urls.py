53610a2cedce846dc7f662f4aae2a060c1a3e81f
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2023-05-23 20:35:28 -0400

    add calalog

--- book_swap/urls.py
+++ book_swap/urls.py
@@ -19,31 +19,21 @@ from django.contrib import admin
 # Use include() to add URLS from the catalog application and authentication system
 from django.urls import include
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-]
-
-
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
 #Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
-urlpatterns += [
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-]
 
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 #Add Django site authentication urls (for login, logout, password management)
