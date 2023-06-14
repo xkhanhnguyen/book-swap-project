1998072335eb40e71964639e66ed31b590ae8eaf
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2023-06-14 10:59:18 -0400

    test cases

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,7 +10,7 @@ from django.forms import ModelForm
 from catalog.models import BookInstance
 
 class RenewBookForm(forms.Form):
-    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3). For practice only, need to update this to sth #")
+    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3). For practice only, need to update this to sth")
     
     def clean_renewal_date(self):
         data = self.cleaned_data['renewal_date']
